82e45b440e5233a7b7b37363813ea53db4365c7d
akula <akulinind@gmail.com>
2022-08-08 09:54:50 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dmytro Akulinin on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
